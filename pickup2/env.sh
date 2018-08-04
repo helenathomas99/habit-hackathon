8a1189adcfbb6dd9c698a645d459100f05792aa7
helenathomas99 <helenathomas15@gmail.com>
2018-08-03 23:37:13 -0700

    saved changed this time

---
+++ pickup2/env.sh
@@ -0,0 +1,2 @@
+export MONGODB_URI="mongodb://helena:horizons2018@ds039145.mlab.com:39145/habit-hackathon"
+export NGROK="http://e9aa7b6a.ngrok.io"
---
+++ pickup2/models.js
@@ -0,0 +1,17 @@
+let mongoose  = require('mongoose');
+let Schema = mongoose.Schema;
+
+//contact model
+let UserSchema = new Schema({
+    username: String,
+    password: String,
+});
+
+let User =  mongoose.model('User', UserSchema)
+
+
+
+
+module.exports = {
+  User: User
+}
